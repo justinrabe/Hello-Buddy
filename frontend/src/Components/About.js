5c5acff12ec8d09c33067dcdab8806ffe90daaea
Justin <jtr342@gmail.com>
2024-04-25 21:40:03 -0700

    Added styling for about

--- frontend/src/Components/About.js
+++ frontend/src/Components/About.js
@@ -9,16 +9,20 @@ function About() {
             <Link to="/" className="logo-link">
                 <img className="logo-container" src={logo} alt="Hello Buddy!" />
             </Link>
-            <h1>Why did we create this app?</h1>
-            <h2>
+            <h1 className="about-subtitle">Why did we create this app?</h1>
+            <h2 className="about-content">
             We affectionately refer to all our pets as "Buddies," which inspired the name of our app, HELLO BUDDY! 
-            Just as we greet our pets with a warm "hello" when we return home, our app embodies that same spirit of connection and companionship.
+            Just as we greet our pets with a warm "hello" when we return home, our app embodies that same spirit of connection and companionship. <br /><br />
 
-            When we return home, it's second nature to seek out our furry companions, sharing with them the events of our day and inquiring about theirs. Engaging in these conversations, though they may be one-sided, brings a sense of connection as if our pets comprehend our words as much as we understand their silent (or not-so-silent) gestures. There's a unique comfort in confiding in our loyal friends who love us unconditionally, their presence warming our hearts in ways only they can.
+            When we return home, it's second nature to seek out our furry companions, sharing with them the events of our day and inquiring about theirs. 
+            Engaging in these conversations, though they may be one-sided, brings a sense of connection as if our pets comprehend our words as much as we understand their silent (or not-so-silent) gestures. 
+            There's a unique comfort in confiding in our loyal friends who love us unconditionally, their presence warming our hearts in ways only they can. <br /><br />
 
-            But HELLO BUDDY! goes beyond mere conversation—it's a tool for empowerment. With features like Buddy customization and virtual companionship for pets who have passed on, we're committed to ensuring that every user can experience the joy of connecting with their furry friends, both present and past.
+            But HELLO BUDDY! goes beyond mere conversation—it's a tool for empowerment. 
+            With features like Buddy customization and virtual companionship for pets who have passed on, we're committed to ensuring that every user can experience the joy of connecting with their furry friends, both present and past. <br /><br />
 
-            Looking ahead, our vision for HELLO BUDDY! includes empowering users to create their own digital companions. Share the cherished memories and distinctive traits of your beloved pet, and HELLO BUDDY!     will bring them to virtual life, courtesy of Google Gemini. Currently, we've personally designed the Buddy icons, with plans to introduce customization features, allowing users to recreate their pets faithfully and engage in meaningful conversations with them.
+            Looking ahead, our vision for HELLO BUDDY! includes empowering users to create their own digital companions. Share the cherished memories and distinctive traits of your beloved pet, and HELLO BUDDY!     
+            will bring them to virtual life, courtesy of Google Gemini. Currently, we've personally designed the Buddy icons, with plans to introduce customization features, allowing users to recreate their pets faithfully and engage in meaningful conversations with them. 
 
             </h2>
         </div>
